65ce99d391809add9cb0cc6961affef462be891c
Emilyuwu0 <tbarrera679@gmail.com>
2024-08-23 17:36:25 -0500

    Hero 🧡

---
+++ src/api/characters.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export default function ApiDBZCharacters() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      fetch("https://dragonball-api.com/api/characters")
+        .then((response) => response.json())
+        .then((data) => setData(data.items)); // Guardar los datos en el estado local
+    }, []);
+
+    return (
+      <div>
+          <ul>
+            {data.map((item) => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
+      </div>
+    )
+}
+
