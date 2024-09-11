e5b43babf091660a64503701ce921d05eeb81b67
Emilyuwu0 <tbarrera679@gmail.com>
2024-09-11 18:42:33 -0500

    Fix 🧡

--- src/components/Home/Planets/index.js
+++ src/components/Home/Planets/index.js
@@ -4,9 +4,22 @@ import TitleUi from "../../Ui/Title/Title";
 import "./index.css";
 export default function Planets() {
   return (
+    <div className="img-planets">
     <div className="parent-divs-planets ">
+      <div className="content-planets-div">
       <div className="one-div-planets ">
-        <div className="main">
+      <span className="title-generic">Planetas</span>
+        <p>
+          En el universo de "Dragon Ball", los planetas juegan un papel crucial
+          en la narrativa y el desarrollo de los personajes. La serie, creada
+          por Akira Toriyama, presenta una variedad de mundos, cada uno con sus
+          propias características, habitantes y culturas.
+        </p>
+        <a  className="margin-top-min" href="#">Ver todos los planetas</a>
+      </div>
+      <div className="two-div-planets">
+        <div className="div-planets">
+          <div className="main">
           <div className="randar-box">
             <input type="checkbox" checked className="randar-checkbox" />
             <div className="randar-top"></div>
@@ -20,20 +33,9 @@ export default function Planets() {
             </div>
           </div>
         </div>
-      </div>
-      <div className="two-div-planets">
-        <div className="div-planets">
-          <span className="title-generic">Planetas</span>
-        <p>
-          En el universo de "Dragon Ball", los planetas juegan un papel crucial
-          en la narrativa y el desarrollo de los personajes. La serie, creada
-          por Akira Toriyama, presenta una variedad de mundos, cada uno con sus
-          propias características, habitantes y culturas.
-        </p>
-        <a  className="margin-top-min" href="#">Ver todos los planetas</a>
         </div>
-        
+        </div>
       </div>
-    </div>
+    </div></div>
   );
 }
--- src/components/Home/Trans/Trans.js
+++ src/components/Home/Trans/Trans.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
-import "./index.css";
-import TitleUi from "../../Ui/Title/Title";
 
+import TitleUi from "../../Ui/Title/Title";
+import { Link } from "react-router-dom";
+import "./index.css";
+import "../../../styles/Generic.css";
+import Button from "../../Ui/Button/Button";
 export default function Trans() {
   const [dataSaya, setDataSaya] = useState([]);
-const [selectedId, setSelectedId] = useState(null)
+  const [selectedId, setSelectedId] = useState(null);
   useEffect(() => {
     fetch("https://dragonball-api.com/api/characters?race=God&page=1&limit=4")
       .then((response) => response.json())
@@ -16,24 +19,52 @@ const [selectedId, setSelectedId] = useState(null)
 
   return (
     <div className="div-father">
-      <div className="div-title">
-        <TitleUi titleUi={"Razas"} />
-      </div>
+      <div className="trans-div">
+        <div className="content-trans-div">
+          <div className="div-trans">
+            <div className="div-title">
+          
 
-      <div className="card">
-        {dataSaya.map((item) => (
-          <p
-            key={item.id}
-            style={{
-              backgroundImage: `url(${item.image}), url(../../../../../assets/bg-race.jpg)`,
-              backgroundSize: "cover",
-            }}
-          >
-            <span>{item.name}</span>
-          </p>
-        ))}
+              <div class="book">
+                {dataSaya.map((item) => (
+                  <>
+                    <div className="card-back">
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="image-card"
+                      />
+                      <p className="center-text name-card-trans">{item.name}</p>
+                    </div>
+                  </>
+                ))}{" "}
+                <div class="cover"></div>
+              </div>
+            </div>
+          </div>
+
+          <div className="div-trans-2 ">    <TitleUi titleUi={"Razas"} />
+            <p className="text-trans-description">
+              En Dragon Ball, las distintas razas enriquecen el universo con su
+              diversidad de poderes y culturas, pero una de las más fascinantes
+              es la raza de los dioses, quienes están por encima de todos en la
+              jerarquía cósmica. Los dioses de la destrucción, como Bills
+              (Beerus), son deidades encargadas de mantener el equilibrio del
+              universo, destruyendo planetas o civilizaciones cuando es
+              necesario. Aunque poseen un poder abrumador, su rol no es ser
+              malignos, sino preservar el orden natural de las cosas. Los
+              acompañan los ángeles, seres aún más poderosos y sabios, como
+              Whis, quienes no solo sirven como mentores, sino también como
+              guías de los dioses.
+            </p>
+            
+            <Link>
+            Vamos allí
+           {/* <Button titleButton={"Vamos allí"} /> */}
+           </Link>
+          </div>
+        </div>
       </div>
-      <a   href="#">Ver mas</a>
     </div>
   );
 }
